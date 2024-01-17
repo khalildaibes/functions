0d19105aee01f4b97ea252640411f5a26e558da8
khalildaibes <khalildaibes1@gmail.com>
2024-01-17 15:08:09 +0200

    first commit

---
+++ functions/main.py
@@ -0,0 +1,62 @@
+# Welcome to Cloud Functions for Firebase for Python!
+# To get started, simply uncomment the below code or create your own.
+# Deploy with `firebase deploy`
+# The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+from firebase_functions import firestore_fn, https_fn
+from firebase_functions.firestore_fn import (
+  on_document_created,
+  on_document_deleted,
+  on_document_updated,
+  on_document_written,
+  Event,
+  Change,
+  DocumentSnapshot,
+)
+# The Firebase Admin SDK to access Cloud Firestore.
+from firebase_admin import initialize_app, firestore
+import google.cloud.firestore
+#
+#
+app = initialize_app()
+
+@https_fn.on_request()
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    return https_fn.Response("Hello world!")
+
+@firestore_fn.on_document_updated(document="Users/*")
+def sendnotification(event: firestore_fn.Event[Change[DocumentSnapshot]]):
+    if event.data is None:
+        return
+        
+    try:
+        original = event.data.get("uid")
+    except KeyError:
+        # No "original" field, so do nothing.
+        return
+
+    # Set the "uppercase" field.
+    print(f"Uppercasing {event.params['pushId']}: {original}")
+    upper = original.upper()
+    event.data.reference.update({"uppercase": upper})
+    return
+
+
+@firestore_fn.on_document_created(document="Users/*")
+def makeuppercase(event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None]) -> None:
+    """Listens for new documents to be added to /Users. If the document has
+    an "original" field, creates an "uppercase" field containg the contents of
+    "original" in upper case."""
+
+    # Get the value of "original" if it exists.
+    if event.data is None:
+        return
+    try:
+        original = event.data.get("token")
+    except KeyError:
+        # No "original" field, so do nothing.
+        return
+
+    # Set the "uppercase" field.
+    print(f"Uppercasing {event.params['pushId']}: {original}")
+    upper = original.upper()
+    event.data.reference.update({"uppercase": upper})
